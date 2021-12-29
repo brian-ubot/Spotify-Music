0751681438a4665024acc906b56a94a688f1bfdd
KaisenXXD <93875452+KaisenXXD@users.noreply.github.com>
2021-12-29 23:29:29 +0700

    Umm

--- plugins/play.py
+++ plugins/play.py
@@ -1,4 +1,3 @@
-
 import os
 from os import path
 from typing import Callable
@@ -84,6 +83,12 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
                 f = await aiofiles.open("background.png", mode="wb")
                 await f.write(await resp.read())
                 await f.close()
+async def generate_cover(title, thumbnail):
+    async with aiohttp.ClientSession() as session, session.get(thumbnail) as resp:
+          if resp.status == 200:
+              f = await aiofiles.open("background.png", mode="wb")
+              await f.write(await resp.read())
+              await f.close()
 
     image1 = Image.open("./background.png")
     image2 = Image.open("etc/foreground.png")
